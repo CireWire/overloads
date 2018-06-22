c40c1fbf0e0fe8e94336b4e22ea0312a462dd24d
CireWire <djcire956@gmail.com>
2018-06-21 23:42:35 -0500

    Create overload.cs

---
+++ overload.cs
@@ -0,0 +1,17 @@
+using System;
+
+class MainClass 
+{
+  public static void Print(int a)
+  {
+    Console.WriteLine(a*a);
+  }
+  public static void Print(double a)
+  {
+    Console.WriteLine(a*a);
+  }
+  public static void Main () 
+  {
+    Print(3);
+  }
+}
